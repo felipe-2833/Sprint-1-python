3c001e3ab898af7242afbf09563e9b64112bc7eb
Felipe-2833 <felipe.elucidator@gmail.com>
2024-05-21 19:01:31 -0300

    sprint 2 concluida

--- functions.py
+++ functions.py
@@ -1,21 +1,45 @@
 #import de bibliotecas
 import random
-# declaração de variaveis globais
-size = 20
-n_agendamento = 0
-problema  = "vago"
-modelo_carro = "vago"
-data = "07/05/2024"
-data0 = "07/05/2024"
-dataop1 = "23/05/2024"
-dataop2 = "30/05/2024"
-data1 = "23/05/2024"
-data2 = "30/05/2024"
+from datetime import date, timedelta
+from settings import *  
+
+# Função separador
+def separador():
+    print("-" * 88)
+    print("\n")
+    
+# Função para o header
+def header():
+    separador()
+    print("-" * 88)
+    print("|*Logo*  HOME  MV  AGENDAR  SOBRE NÒS     (Digíte sua pergunta...   ) *INCONE PERGUNTA*|")
+    separador()
+    print("\t","-" * 65)
+    
+# Função verifica marca e modelo
+def verifica_marca_modelo():
+    marca = input("\tDigite a marca do seu carro: ").lower()
+    if marca not in MARCA_CARROS:
+        print("\t\nMarca não encontrada.")
+        return "","",False
+    
+    indice_marca = MARCA_CARROS.index(marca)
+    modelo = input(f"\tDigite o modelo do seu carro ({', '.join(MODELOS_CARROS[indice_marca])}): ").lower()
+    if modelo not in MODELOS_CARROS[indice_marca]:
+        print("\t\nModelo não encontrado.")
+        return "","", False
+    
+    return marca, modelo, True
+
+# Função gera oficina aleatoria
+def escolhe_oficina_aleatoria():
+    return random.choice(OFICINAS_MECANICAS)
+
 # Função para desenhar um quadrado com o tamanho especificado, para utilizar como foto
-def foto(size):
+def foto(SIZE):
     print("\n\tTire uma foto do problema do carro")
     for i in range(10):
-        print("\t\t\t\t\t\t\t\t",'*' * size)
+        print("\t\t\t\t\t\t\t\t",'*' * SIZE)
     print("\n\tObrigado!")
 
 # Função para agendar e forncer o codigo de agendamento
@@ -24,10 +48,10 @@ def agendamento(tipo_problema):
         inf = input("\n\t Gostaria de agendar com nossos mecanicos?(sim ou não):")
         if inf == "sim":
             ramdom = random.randint(1,100)
-            n_agendamento = int(ramdom) 
-            print(f"\n\tOK foi agendado! Seu numero de agendamento é {n_agendamento}")
+            N_AGENDAMENTO = int(ramdom) 
+            print(f"\n\tOK foi agendado! Seu numero de agendamento é {N_AGENDAMENTO}")
             print(f"\tAgora vá/ou retorne a pagina de agendamento e digite seu numero de\n\t agendamento para informações")
-            return n_agendamento
+            return N_AGENDAMENTO
         elif inf == "não":
             print("\tOK, até a proxima!")
             break
@@ -46,28 +70,28 @@ def tipo_problema_():
             case '1':
                 foto(20)
                 print("\n\t Certo!Segundo a foto tirada o problema é de superaquecimento do carro,\n\t pode ser um problema sério, pois pode danificar o motor e outros \n\tcomponentes importantes do veículo...")
-                problema = "Poblema Motor"
-                n_agendamento = agendamento(problema)
-                return str(n_agendamento), problema
+                PROBLEMA = "Poblema Motor"
+                N_AGENDAMENTO = agendamento(PROBLEMA)
+                return str(N_AGENDAMENTO), PROBLEMA
             case '2':
                 foto(20)
                 print("\n\t Certo!Segundo a foto tirada o problema é de dificuldade de troca de marcha,\n\t será necessário uma troca dos sincronizadores desgastados...")
-                problema = "Problema Transmissão"
-                n_agendamento = agendamento(problema)
-                return str(n_agendamento), problema
+                PROBLEMA = "Problema Transmissão"
+                N_AGENDAMENTO = agendamento(PROBLEMA)
+                return str(N_AGENDAMENTO), PROBLEMA
             case '3':
                 foto(20)
                 print("\n\t Certo!Segundo a foto tirada o problema é da bateria do carro\n\t será necesserio troca...")
-                problema = "Falha Elétrica"
-                n_agendamento = agendamento(problema)
-                return str(n_agendamento), problema
+                PROBLEMA = "Falha Elétrica"
+                N_AGENDAMENTO = agendamento(PROBLEMA)
+                return str(N_AGENDAMENTO), PROBLEMA
 
             case '4':
                 foto(20)
                 print("\n\t Certo!Segundo a foto tirada o problema está no desgaste das pastilhas,\n\t será necesserio troca...")
-                problema = "Problemas freios"
-                n_agendamento = agendamento(problema)
-                return str(n_agendamento), problema
+                PROBLEMA = "Problemas freios"
+                N_AGENDAMENTO = agendamento(PROBLEMA)
+                return str(N_AGENDAMENTO), PROBLEMA
             case _:
                 print("Escolha entre 1, 2, 3 ou 4")
             
@@ -77,22 +101,20 @@ def cancelar_agendamento(n_agendameto):
     confirm = input("\tGostaria de cancelar seu agendamento?(sim ou não):")
     if confirm == "sim":
         print("\tCancelamento realizado com sucesso!")
-        print("-" * 88)
-        print("\n")
+        separador()
         return 0
     elif confirm == "não":
         print("OK, até!")
-        print("-" * 88)
-        print("\n")
+        separador()
         return n_agendameto
     else:
         input("Informação inválida ")
         return n_agendameto
 
 # Função para mudar data de agendamento
-def mudar_data(data,dataop1,dataop2,data0,problema,modelo_carro):
+def mudar_data(data,dataop1,dataop2,problema,marca_carro,modelo_carro,mecanica):
     while True:
-        print(f"\n\tInformações do agendamento:\n\t\t -Data: {data}\n\t\t -Modelo do carro: {modelo_carro}\n\t\t -Problema: {problema} \n\t\t -Mecânica: Seu Zê")
+        print(f"\n\tInformações do agendamento:\n\t\t -Data: {data}\n\t\t -Marca do carro: {marca_carro}\n\t\t -Modelo do carro: {modelo_carro}\n\t\t -Problema: {problema} \n\t\t -Mecânica: {mecanica}")
         confirm_data = input("\n\tGostaria de mudar a data?(sim ou não):")
         if confirm_data == "sim":
             print(f"\tOK, temos essas datas disponiveis: \n\t1 -{dataop1} \n\t2 -{dataop2}")
@@ -101,32 +123,32 @@ def mudar_data(data,dataop1,dataop2,data0,problema,modelo_carro):
                 case "0":
                     return data
                 case "1":
-                    if data == data0:
-                        data = data1
-                        dataop1 = data0
-                        dataop2 = data2
-                    elif data == data1:
-                         data = data0
-                         dataop1 = data1
-                         dataop2 = data2
+                    if data == DATA0:
+                        data = DATA1
+                        dataop1 = DATA0
+                        dataop2 = DATA2
+                    elif data == DATA1:
+                         data = DATA0
+                         dataop1 = DATA1
+                         dataop2 = DATA2
                     else:
-                         data = data0
-                         dataop1 = data1
-                         dataop2 = data2
+                         data = DATA0
+                         dataop1 = DATA1
+                         dataop2 = DATA2
                     continue
                 case "2":
-                    if data == data0:
-                        data = data2
-                        dataop1 = data0
-                        dataop2 = data1
-                    elif data == data1:
-                         data = data2
-                         dataop1 = data0
-                         dataop2 = data1
+                    if data == DATA0:
+                        data = DATA2
+                        dataop1 = DATA0
+                        dataop2 = DATA1
+                    elif data == DATA1:
+                         data = DATA2
+                         dataop1 = DATA0
+                         dataop2 = DATA1
                     else:
-                         data = data1
-                         dataop1 = data0
-                         dataop2 = data2
+                         data = DATA1
+                         dataop1 = DATA0
+                         dataop2 = DATA2
                     continue
                 case _:
                     print("\tEscolha entre 0, 1 ou 2")
@@ -146,20 +168,17 @@ def duvidas():
         match duv:
             case '0':
                 print("OK, até!")
-                print("-" * 88)
-                print("\n")
+                separador()
                 return 0
             case '1':
                 print("\n\tCerto!Entre na pagina Mecânico Virtual, faça um diagnostico de seu \n\tproblema. Marque uma consulta e grave seu numero de agendameto para poder \n\tver suas informações.")
                 confirm = input("\n\tGostaria de tirar mais alguma duvida?(sim ou não): ")
                 if confirm == "sim":
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 4
                 elif confirm == "não":
                         print("OK, até!")
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 0
                 else:
                         input("Informação inválida ")
@@ -168,13 +187,11 @@ def duvidas():
                 print("\n\tCerto!É possivel cancelar seu agendamento na pagina AGENDAMENTO.")
                 confirm = input("\n\tGostaria de tirar mais alguma duvida?(sim ou não): ")
                 if confirm == "sim":
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 4
                 elif confirm == "não":
                         print("OK, até!")
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 0
                 else:
                         input("Informação inválida ")
@@ -183,13 +200,11 @@ def duvidas():
                 print("\n\tCerto!Entre na pagina de AGENDAMENTO, coloque seu numero de agendamento \n\te escolha a opção CANCELAR AGENDAMENTO escrevendo cancelar no indicado;")
                 confirm = input("\n\tGostaria de tirar mais alguma duvida?(sim ou não): ")
                 if confirm == "sim":
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 4
                 elif confirm == "não":
                         print("OK, até!")
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 0
                 else:
                         input("Informação inválida ")
@@ -198,13 +213,11 @@ def duvidas():
                 print("\n\tCerto!É apenas possivel realizar 1 agendamento por vez e então \n\tvizualizalo na pagina de AGENDAMENTO.")
                 confirm = input("\n\tGostaria de tirar mais alguma duvida?(sim ou não): ")
                 if confirm == "sim":
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 4
                 elif confirm == "não":
                         print("OK, até!")
-                        print("-" * 88)
-                        print("\n")
+                        separador()
                         return 0
                 else:
                         input("Informação inválida ")
--- sprintPython.py
+++ sprintPython.py
@@ -1,24 +1,12 @@
 #Import das funções
 from functions import *
-#variaveis globais
-pagina = 0
-size = 20
-n_agendamento = 0
-problema  = "vago"
-modelo_carro = "vago" 
-data = "07/05/2024"
-data0 = "07/05/2024"
-data1 = "23/05/2024"
-data2 = "30/05/2024"              
+from settings import *
+from datetime import date, timedelta
+        
 
 #PAGINA PRINCIPAL/ HOME
-while pagina == 0:
-    print("\n")
-    print("-" * 88)
-    print("|*Logo*  HOME  MV  AGENDAR  SOBRE NÒS     (Digíte sua pergunta...   ) *INCONE PERGUNTA*|")
-    print("-" * 88)
-    print("\n")
-    print("\t","-" * 65)
+while PAGINA == 0:
+    header()
     print("\t PROJETO")
     print("\t  O projeto “Mecânico Virtual” é uma solução criada com \n\t  o intuito de facilitar a comunicação entre cliente e profissional, \n\t  como também tornar todo o processo mais simples, \n\t  com ambas as partes tendo suas necessidades atendidas o tempo todo.")
     print("\n")
@@ -28,87 +16,66 @@ while pagina == 0:
     match opcao:
         #MECÂNICO VIRTUAL 
         case "1":
-           pagina = 1
-           while pagina == 1: 
-               print("-" * 88)
-               print("\n")
-               print("-" * 88)
-               print("|*Logo*  HOME  MV  AGENDAR  SOBRE NÒS     (Digíte sua pergunta...   ) *INCONE PERGUNTA*|")
-               print("-" * 88)
-               print("\n")
-               print("\t","-" * 65)
+           PAGINA = 1
+           while PAGINA == 1: 
+               header()
                print("\t\t\t MECÂNICO VIRTUAL")
                print("\n\tOlá, sou seu mecânico virtual.")
-               modelo_carro = input("\n\tDigite o modelo de seu carro: ")
+               MARCA_CARRO, MODELO_CARRO, eh_valido = verifica_marca_modelo()
+               if not eh_valido:
+                   PAGINA = 0
+                   break
                print("\n\t Aqui estão alguns problemas em que posso te ajudar: ")
                print("\n\t 1 - Problemas de Motor: \n\t\t - Falhas de ignição, superaquecimento, problemas com a \n\t\tbateria ou falhas no sistema de injeção de combustível.")
                print("\n\t 2 - Problemas de Transmissão: \n\t\t - Dificuldades para engatar as marchas ou ruídos \n\t\testranhos durante as mudanças.")
                print("\n\t 3 - Falhas no Sistema Elétrico: \n\t\t - Falhas na bateria, alternador ou arranque.\n\t\tProblemas com sistemas de iluminação, sensores, \n\t\tsistemas de som e outros componentes eletrônicos.")
                print("\n\t 4 - Problemas no Sistema de Freios: \n\t\t - Desgaste nas pastilhas ou discos de freio,\n\t\t vazamentos no sistema hidráulico, ou falhas \n\t\t no sistema de assistência de frenagem.")
                print("\n")
-               n_agendamento, problema = tipo_problema_()
-               pagina = 0
+               N_AGENDAMENTO, PROBLEMA = tipo_problema_()
+               PAGINA = 0
         
         #AGENDAR                
         case "2": 
-           pagina = 2
-           while pagina == 2: 
-               print("-" * 88)
-               print("\n")
-               print("-" * 88)
-               print("|*Logo*  HOME  MV  AGENDAR  SOBRE NÒS     (Digíte sua pergunta...   ) *INCONE PERGUNTA*|")
-               print("-" * 88)
-               print("\n")
-               print("\t","-" * 65)
+           PAGINA = 2
+           while PAGINA == 2: 
+               header()
                print("\t\t\t AGENDAMENTO")
                n = input("\n\t Olá, qual seu numero de agendamento? (se não tiver um, digite 0):")
                if n == "0":
                     print("\n\tVá até a pagina de Mecânico virtual para fazermos um diagnostico \n\tdo seu problema:")
-                    pagina = 0
-               elif n == n_agendamento:
-                    data = mudar_data(data,data1,data2,data0,problema,modelo_carro)
+                    PAGINA = 0
+               elif n == N_AGENDAMENTO:
+                    mecanica = escolhe_oficina_aleatoria()
+                    DATA = mudar_data(DATA,DATAOP1,DATAOP2,PROBLEMA,MARCA_CARRO,MODELO_CARRO,mecanica)
                     
-                    n_agendamento = cancelar_agendamento(n_agendamento)
-                    pagina = 0        
+                    N_AGENDAMENTO = cancelar_agendamento(N_AGENDAMENTO)
+                    PAGINA = 0        
                else:
                    print("\n\tTipo de infomação não aceitavel!") 
-                   pagina = 0
+                   PAGINA = 0
         
         #SOBRE NÓS
         case "3":
-            pagina = 3
-            while pagina == 3: 
-               print("-" * 88)
-               print("\n")
-               print("-" * 88)
-               print("|*Logo*  HOME  MV  AGENDAR  SOBRE NÒS     (Digíte sua pergunta...   ) *INCONE PERGUNTA*|")
-               print("-" * 88)
-               print("\n")
-               print("\t","-" * 65)
+            PAGINA = 3
+            while PAGINA == 3: 
+               header()
                print("\t\t\t SOBRE NÓS")
                print("\n\t|*LOGO*  ANDRÉ GERALDI MARCOLONGO      RM555285|")
                print("\n\t|*LOGO*  SAMIR HAGE NETO               RM555285|")
                print("\n\t|*LOGO*  FELIPE LEVY STEPHENS FIDELIX  RM556426|")
                confirm = input("\nVoltar para HOME?(Escreva sim para voltar): ")
                if confirm == "sim":
-                    print("-" * 88)
-                    print("\n")
-                    pagina = 0
+                    separador()
+                    PAGINA = 0
                else:
                     input("Informação inválida ")
-                    pagina = 3
+                    PAGINA = 3
         
         #FAZER PERGUNTA
         case "4":
-            pagina = 4
-            while pagina == 4: 
-               print("-" * 88)
-               print("\n")
-               print("-" * 88)
-               print("|*Logo*  HOME  MV  AGENDAR  SOBRE NÒS     (Digíte sua pergunta...   ) *INCONE PERGUNTA*|")
-               print("-" * 88)
-               print("\n")
-               print("\t","-" * 65)
+            PAGINA = 4
+            while PAGINA == 4: 
+               header()
                print("\t\t\t DUVIDAS")
                print("\n\tOlá sou seu assistente virtual, como posso te ajudar?")
                print("\n\t Aqui estão alguns problemas em que posso te ajudar: ")
@@ -117,7 +84,7 @@ while pagina == 0:
                print("\n\t 3 - Como faço para alterar ou cancelar um agendamento existente?: ")
                print("\n\t 4 - Como posso visualizar ou gerenciar meus agendamentos futuros?: ")
                print("\n")
-               pagina = duvidas()
+               PAGINA = duvidas()
                
         #ENCERRAR SESSÃO      
         case "5":
@@ -130,5 +97,4 @@ while pagina == 0:
             print("\n")
             print("Opção invalida!")
             print("Escolha um das opções: 1, 2, 3, 4 ou 5.")
-            print("-" * 88)
             continue
